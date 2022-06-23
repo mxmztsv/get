435a50d3020e61e80574c55ce32ef37a0664824c
hihello80 <107503076+hihello80@users.noreply.github.com>
2022-06-23 14:07:38 -0700

    init

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,9 +1,9 @@
 export function fN(number, digits, toStr) {
   let resultNumber;
-  if (digits === 1) resultNumber = Math.round(number * 10) / 10;
-  else if (digits === 2) resultNumber = Math.round(number * 100) / 100;
-  else if (digits === 3) resultNumber = Math.round(number * 1000) / 1000;
-  else resultNumber = Math.round(number);
+  if (digits === 1) resultNumber = Math.floor(number * 10) / 10;
+  else if (digits === 2) resultNumber = Math.floor(number * 100) / 100;
+  else if (digits === 3) resultNumber = Math.floor(number * 1000) / 1000;
+  else resultNumber = Math.floor(number);
 
   if (toStr) resultNumber = resultNumber.toLocaleString("en-US");
 
